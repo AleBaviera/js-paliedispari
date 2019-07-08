d144727c7766e85b3aa272d2a5041ce3054be527
alebaviera <baviera.alessandra@gmailcom>
2019-07-08 13:55:05 +0200

    prova con array

--- palindrome/main.js
+++ palindrome/main.js
@@ -3,14 +3,34 @@ console.log('cia');
 
 // 1 -Una funzione per capire se la parola è palindroma;
 
-// dichiaro e valorizzo var parola
-var parola = prompt('inserisci una parola');
+// dichiaro var
+var risultato, parola;
+
+// chiedo parola e la metto in array
+parola = [];
+// parola = prompt('inserisci una parola');
 
 // creo funzione
+  function palindroma(){
+    var a = [];
+    a = prompt('inserisci una parola');
+
+    for (var i = 0; i < a.length; i++) {
 
 
-// richiamo la funzione
+      if (a[i] === a[i].reverse()){
+        console.log(a.reverse());
+        return 'è palindroma';
 
+      }
+      else {
+        return ' non è palindroma';
+      }
+    }
+  }
 
+// richiamo la funzione e restituisco risultato
 
-// restituisco risultato
+risultato = palindroma(parola);
+
+console.log(risultato);
