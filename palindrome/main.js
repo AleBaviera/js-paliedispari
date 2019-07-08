2c13d901dafd49d075714ecbcc211a225f7fff32
alebaviera <baviera.alessandra@gmailcom>
2019-07-08 13:23:28 +0200

    cartelle, file e bozza struttura

---
+++ palindrome/main.js
@@ -0,0 +1,16 @@
+console.log('cia');
+
+
+// 1 -Una funzione per capire se la parola è palindroma;
+
+// dichiaro e valorizzo var parola
+var parola = prompt('inserisci una parola');
+
+// creo funzione
+
+
+// richiamo la funzione
+
+
+
+// restituisco risultato
